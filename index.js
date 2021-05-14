dcc20af5c71b947167549a5e34f663939ec94b7d
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-14 15:51:00 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let result;
+  array.forEach((item)=>{
+    if(item.result == "W") result = item.year;
+  });
+  return result;
+}
