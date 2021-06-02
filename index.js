d2b8821702fadc820e387165507312d10def8857
Omer Sabah <omerrj99@gmail.com>
2021-06-02 23:37:15 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+const superbowlWin = (record) => {
+  const result = record.find( record => record.result === "W" )
+  return result ? result.year : undefined
+}
